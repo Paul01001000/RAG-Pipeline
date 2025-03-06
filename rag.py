594dda2a5de215b6714e226c1fd58388748cb802
Paul <paul@Laptop-von-Paul.fritz.box>
2025-03-06 21:02:30 +0100

    Init

--- rag.py
+++ rag.py
@@ -1,2 +1,11 @@
 import sqlite3
 import json
+
+from ollama import chat
+
+def extract_keywords(prompt: str):
+    pass
+
+def main(prompt: str) -> str:
+    keywords = extract_keywords(prompt)
+    
