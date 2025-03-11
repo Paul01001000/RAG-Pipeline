2cbff20ba855a824edee148c25ae4be88d76697d
Paul <paul@Laptop-von-Paul.fritz.box>
2025-03-11 20:50:52 +0100

    Add DB connection

--- rag.py
+++ rag.py
@@ -7,9 +7,24 @@ from typing import List, Dict
 def json_parser(json_obj: str) -> bool:
     pass
 
-def get_articles(db: str) -> List[Dict]:
+def get_articles(db_path: str = "news.db") -> List[Dict]:
     #Gets all articles from the Database
-    pass
+    """
+    DB Data Source:
+    1. Misra, Rishabh. "News Category Dataset." arXiv preprint arXiv:2209.11429 (2022).
+    2. Misra, Rishabh and Jigyasa Grover. "Sculpting Data for ML: The first act of Machine Learning." ISBN 9798585463570 (2021).
+    https://www.kaggle.com/datasets/rmisra/news-category-dataset?resource=download
+    """
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+    query = "SELECT headline,category,short_description,date FROM News LIMIT 20"
+    cursor.execute(query)
+    rows = cursor.fetchall()
+
+    articles = [{"headline":headline, "category":category, "short_description":short_description, "date":date} for headline,category,short_description,date in rows]
+    conn.close()
+    return articles
+
 
 def select_relevant_articles(article: Dict, idx: int, question: str, model: str = "deepseek-r1:1.5b") -> bool:
     #Decides if given article is relevant
